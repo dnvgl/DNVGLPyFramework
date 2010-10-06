a6177beaecdd07eaffbc534c5a3b13ca1b4c193e
Berthold Höllmann <berthold.hoellmann@dnvgl.com>
2010-10-06 14:52:28 +0000

    added functionality to identify planes partly provided by Karsten Stenzel <karsten.stenzel@t-online.de>
    git-svn-id: https://svnsrv/svn/DNVGLPyFramework/trunk@58 c98a7023-1c74-4c12-b3e7-2c1132d2d72c

---
+++ src/tribon/util/planecheck.py
@@ -0,0 +1,73 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+u""" Wrapper for class `Plane` from `tbplanev1` module by Karsten
+Stenzel (<karsten.stenzel@t-online.de>)
+
+:author: `Berthold Hoellmann <hoel@GL-group.com>`__
+:newfield project: Project
+:project: tribonXML converters
+:copyright: Copyright (C) 2010 by Germanischer Lloyd AG"""
+
+from __future__ import absolute_import
+
+#  ID: $Id$
+__date__      = u"$Date$"[5:-1]
+__version__   = "$Revision$"[10:-1]
+__docformat__ = "restructuredtext en"
+
+import itertools
+
+from ..stenzel.tbplanev1 import Plane as tbPlane
+
+class FakeKcsUtil(object):
+    """Fake kcs_util for Plane implementation by Karsten Stenzel.
+"""
+
+    def __init__(self, frmtbl):
+        self.frmtbl = []
+        [ self.frmtbl.extend(zip(ft.unfold(),
+                                 itertools.repeat(ft.spacing)))
+          for ft in frmtbl.positions ]
+        self.frmtbl = [ a + (b,) for a, b in self.frmtbl ]
+
+        print self.frmtbl
+    def coord_to_pos(self, dummy, coor):
+        """Calculate frame position from coordinate.
+"""
+        if coor < self.frmtbl[0][1]:
+            return (True, None, None)
+        lstfrm, lstpos, lstspc = self.frmtbl[0]
+        for frame, pos, spacing in self.frmtbl:
+            if coor < pos:
+                if coor < lstpos-lstspc/2.:
+                    return (True, None, None)
+                elif coor < pos-spacing/2.:
+                    return (None, lstfrm, (coor-lstpos)*1000.)
+                else:
+                    return (None, frame, (coor-pos)*1000.)
+            lstfrm, lstpos, lstspc = frame, pos, spacing
+        if coor < lstpos+lstspc/2.:
+            return (None, lstfrm, (coor-lstpos)*1000.)
+        return (True, None, None)
+
+def injectFrameTable(frmtbl):
+    """inject frametable information into tbPlane
+"""
+    from ..stenzel import tbplanev1
+    tbplanev1.hasTribon = True
+    tbplanev1.kcs_util = FakeKcsUtil(frmtbl)
+
+class Plane(tbPlane):
+
+    def __init__(self, o, w):
+        args = list(o)+list(w)
+        super(Plane, self).__init__(*args)
+
+
+# Local Variables:
+# mode:python
+# mode:flyspell
+# pylint-options: "--output-format=parseable --import-graph=planecheck_import.png --init-hook=\"import sys;sys.path.append('..')\" "
+# compile-command:"make -C ../../../ tribon.util.planecheck_test"
+# End:
