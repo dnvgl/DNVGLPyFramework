35b82efd318865285bd28d972bf9f1e2c84b16f6
Berthold Höllmann <berthold.hoellmann@dnvgl.com>
2015-03-10 14:27:16 +0000

    Finished version 0.4.4.
    git-svn-id: https://svnsrv/svn/DNVGLPyFramework/trunk@144 c98a7023-1c74-4c12-b3e7-2c1132d2d72c

--- setup.py
+++ setup.py
@@ -41,7 +41,6 @@ if __name__ == '__main__':
                      'Intended Audience :: Developers',
                      'Operating System :: POSIX',
                      'Operating System :: Microsoft :: Windows',
-                     'Operating System :: MacOS :: MacOS X',
                      'Topic :: Software Development :: Libraries',
                      'Topic :: Utilities',
                      'Programming Language :: Python',
