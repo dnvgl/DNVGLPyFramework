36000f4ac8f95f053f37c0a420ca258762ef99cf
Berthold Höllmann <berthold.hoellmann@dnvgl.com>
2018-02-01 10:24:03 +0000

    Summary: Added release helper script.
    git-svn-id: https://svnsrv/svn/DNVGLPyFramework/trunk@193 c98a7023-1c74-4c12-b3e7-2c1132d2d72c

---
+++ release.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+# Copyright © 2018 by DNV GL SE
+
+# Task  : Release latest Fortran2CHeader version
+
+# Author: Berthold Höllmann <berthold.hoellmann@dnvgl.com>
+
+# ID: $Id$
+author="$Author$"
+date="$Date$"
+version="$Revision$"
+
+set -e
+
+# Generate source distribution files.
+python setup.py sdist --format=zip,gztar
+
+# Generate binary distribution files (egg and wheel) for
+# python2.7 and 3.4.
+tox
+
+# connect to devpi (local pypi) server.
+devpi use http://srverc.germanlloyd.org/devpi/dnvgl/dist
+
+# upload distribution files to devpi server.
+devpi upload dist/Fortran2CHeader-$(cat version.txt)*
+
+# Local Variables:
+# mode: shell-script
+# coding: utf-8
+# compile-command: "sh release.sh"
+# End:
