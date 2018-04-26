1e96b63c7fcf9e9b8cae631028986e0de75f03cc
Berthold Höllmann <berthold.hoellmann@dnvgl.com>
2018-04-26 13:30:59 +0000

    Trying to improve subprocess monkeypatching.
    git-svn-id: https://svnsrv/svn/DNVGLPyFramework/trunk@208 c98a7023-1c74-4c12-b3e7-2c1132d2d72c

--- lib/dnvgl/setup_utils/test/test_version.py
+++ lib/dnvgl/setup_utils/test/test_version.py
@@ -9,6 +9,7 @@ from __future__ import (
 # Standard libraries.
 import os
 import sys
+import functools
 import subprocess
 
 # Third party libraries.
@@ -38,11 +39,19 @@ def ver_string(request):
     return request.param
 
 
+def get_check_output(rev):
+
+    def check_output(rev, *args, **kw):
+        if (isinstance(rev, str) and ':' in rev):
+            return rev
+        return bytes("1:{}".format(rev).encode("utf-8"))
+
+    return functools.partial(check_output, rev)
+
+
 def test_ver_explicit_path(tmpdir, monkeypatch, ver_string):
     ver, rev, ref, rel = ver_string
-    monkeypatch.setattr(subprocess, "check_output",
-                        lambda x, *arg, **kw:
-                        bytes("1:{}".format(rev).encode("utf-8")))
+    monkeypatch.setattr(subprocess, "check_output", get_check_output(rev))
     v = tmpdir.join('version.txt')
     v.write(ver)
     probe = version.Version(v)
@@ -56,8 +65,7 @@ def test_ver_explicit_path(tmpdir, monkeypatch, ver_string):
 def test_ver_explicit_path_env(tmpdir, monkeypatch, ver_string):
     ver, rev, ref, rel = ver_string
     monkeypatch.setenv('SVN_REVISION', rev)
-    monkeypatch.setattr("subprocess.check_output",
-                        lambda x, *arg, **kw: bytes("1:666"))
+    monkeypatch.setattr(subprocess, "check_output", get_check_output("1:666"))
     v = tmpdir.join('version.txt')
     v.write(ver)
     probe = version.Version(v)
@@ -73,9 +81,7 @@ def test_ver_implicit_path(tmpdir, monkeypatch, ver_string):
     v = tmpdir.join('version.txt')
     v.write(ver)
     monkeypatch.setattr("sys.argv", (v.strpath, ))
-    monkeypatch.setattr("subprocess.check_output",
-                        lambda x, *arg, **kw:
-                        bytes("1:{}".format(rev).encode("utf-8")))
+    monkeypatch.setattr(subprocess, "check_output", get_check_output(rev))
     probe = version.Version()
     if rel is not None:
         assert probe() == ref
@@ -90,8 +96,7 @@ def test_ver_implicit_path_env(tmpdir, monkeypatch, ver_string):
     v.write(ver)
     monkeypatch.setenv('SVN_REVISION', rev)
     monkeypatch.setattr("sys.argv", (v.strpath, ))
-    monkeypatch.setattr("subprocess.check_output",
-                        lambda x, *arg, **kw: bytes("1:666"))
+    monkeypatch.setattr(subprocess, "check_output", get_check_output("1:666"))
     probe = version.Version()
     if rel is not None:
         assert probe() == ref
@@ -105,9 +110,7 @@ def test_release(tmpdir, monkeypatch, ver_string):
     v = tmpdir.join('version.txt')
     v.write(ver)
     monkeypatch.setattr("sys.argv", (v.strpath, ))
-    monkeypatch.setattr("subprocess.check_output",
-                        lambda x, *arg, **kw:
-                        bytes("1:{}".format(rev).encode("utf-8")))
+    monkeypatch.setattr(subprocess, "check_output", get_check_output(rev))
     probe = version.Version()
     if rel is not None:
         assert probe.release == rel
@@ -119,8 +122,7 @@ def test_release_env(tmpdir, monkeypatch, ver_string):
     v.write(ver)
     monkeypatch.setenv('SVN_REVISION', rev)
     monkeypatch.setattr("sys.argv", (v.strpath, ))
-    monkeypatch.setattr("subprocess.check_output",
-                        lambda x, *arg, **kw: bytes("1:666"))
+    monkeypatch.setattr(subprocess, "check_output", get_check_output("1:666"))
     probe = version.Version()
     if rel is not None:
         assert probe.release == rel
@@ -145,7 +147,7 @@ def test_parse_template(tmpdir, monkeypatch):
     v.write("1.2.3")
     tmpl = tmpdir.join('parse_template_test.in')
     tmpl.write("""Just a test
-{{ version }}
+{{ version }}\n
 """)
     monkeypatch.setattr("sys.argv", (v.strpath, ))
     with v.dirpath().as_cwd():
@@ -153,7 +155,8 @@ def test_parse_template(tmpdir, monkeypatch):
         probe.parse_template(str(tmpl))
         assert tmpdir.join("parse_template_test").read() == '''\
 Just a test
-1.2.3'''
+1.2.3
+'''
 
 # Local Variables:
 # mode: python
